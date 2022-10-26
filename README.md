a0f624b2cc7f0a38623e3a7e4adccb0344975439
Preide24 <101293511+Preide24@users.noreply.github.com>
2022-10-26 20:00:17 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Luciano-Git---Capitulo-1
+Repositorio "Luciano - Git" - Capítulo 1: Primer repositorio de la clase de JavaScript en el CFP 408, Buenos Aires.
---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+//Definir dos variables constantes
+const primerNumero = 30;
+const segundoNumero = 70;
+//Llamar a la función, sumar, pasar los valores y capturar el resultado
+var ver = sumar(primerNumero,segundoNumero);
+console.log(ver);
+//Llamar a la función, restar, pasar los valores y capturar el resultado
+/* Llamar a la función, comparar los valores, pasar los valores, fijarse
+que el primer valor sea mayor y capturar el resultado */
+//Imprimir los resultados en consola
+//Imprimir el tipo de resltado en consola
+
+const suma = sumar(1,2);
+console.log(suma);
+function sumar(num1,num2)
+{
+    let operacion = num1 + num2
+    return operacion;
+}
